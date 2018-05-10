62972b5523e21c9b3656a3a73bebae15520446d5
Daniel Kavanagh <dan.kava13@gmail.com>
2018-05-10 13:40:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port     = 8000;
 app.use(express.static(path.join(__dirname, "./client/dist")));
 app.use(bp.json());
 app.use(session({
-    secret: "lolololololol secrets",
+    secret: "testsecret",
     saveUninitialized: false,
     resave: false,
 }));
